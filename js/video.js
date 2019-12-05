72cf6559bee27aba1bab4516e23cbc0d2ffdff3a
PySom <nwisuchisom@gmail.com>
2019-12-05 08:32:23 +0100

    update

--- js/video.js
+++ js/video.js
@@ -2,6 +2,7 @@ const Video = () => {
     const video = document.createElement('video');
     video.controls = 'controls';
     video.src = './videos/vids.mp4'
+    video.style.width = "100%";
     return video;
 }
 
